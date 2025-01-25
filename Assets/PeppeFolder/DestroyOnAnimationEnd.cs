225eb8b7a00679046b58bbb741bc0ec65babc56b
PeppeAb <pio-foco@hotmail.it>
2025-01-26 00:47:27 +0100

    animation score Bubble

---
+++ Assets/PeppeFolder/DestroyOnAnimationEnd.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+public class DestroyOnAnimationEnd : MonoBehaviour
+ {
+   private Animator animator;
+       void Start()
+    {
+        Animator animator = GetComponent<Animator>();
+
+        if (animator != null)
+        {
+            animator.Play(animator.GetCurrentAnimatorClipInfo(0)[0].clip.name);
+            Destroy(gameObject, animator.GetCurrentAnimatorStateInfo(0).length);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
